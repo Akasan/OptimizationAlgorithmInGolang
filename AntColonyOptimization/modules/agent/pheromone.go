2525c5321dbe47b7d431b754eb3f79b3ce7b30fd
Daisuke AKAGAWA <daisuke1024akagawa@gmail.com>
2023-02-27 22:28:20 +0900

    Implement pheromone base

--- AntColonyOptimization/modules/agent/pheromone.go
+++ AntColonyOptimization/modules/agent/pheromone.go
@@ -1,7 +1,41 @@
 package agent
 
-struct Pheromone struct {
- Pheromone [][]float32 
+
+type ReduceRate float32
+type PheromoneType float32
+type CitySize int
+
+type Pheromone struct {
+  ReduceRate ReduceRate
+  CitySize CitySize
+  Pheromone [][]PheromoneType 
 }
 
 
+func (p *Pheromone) Init(initialPheromone PheromoneType){
+  for i := 0; i<int(p.CitySize); i++{
+    p.Pheromone = append(p.Pheromone, make([]PheromoneType, p.CitySize))
+  }
+
+  for i := 0; i<int(p.CitySize); i++{
+    for j := i+1; j<int(p.CitySize); j++{
+      p.Pheromone[i][j] = initialPheromone
+      p.Pheromone[j][i] = initialPheromone
+    }
+  }
+}
+
+
+func (p *Pheromone) Reduce(){
+  for i := 0; i<int(p.CitySize); i++{
+    for j := i+1; j<int(p.CitySize); j++{
+      p.Pheromone[i][j] = p.Pheromone[i][j] * PheromoneType(p.ReduceRate)
+      p.Pheromone[j][i] = p.Pheromone[j][i] * PheromoneType(p.ReduceRate)
+    }
+  }
+}
+
+func (p *Pheromone) AddPheromone(c1 int, c2 int, additionalPheromone PheromoneType){
+  p.Pheromone[c1][c2] += additionalPheromone
+  p.Pheromone[c2][c1] += additionalPheromone
+}
--- AntColonyOptimization/test.go
+++ AntColonyOptimization/test.go
@@ -7,10 +7,15 @@ import (
 
 func main() {
 	colony := agent.Colony{ColonySize: 10}
-	colony.Initialize()
-
-	for i, a := range colony.Agents {
-		fmt.Println(i)
-		fmt.Println(a)
+	pheromone := agent.Pheromone{
+		ReduceRate: 0.99,
+		CitySize:   10,
 	}
+	pheromone.Init(0.5)
+	pheromone.Reduce()
+	pheromone.Reduce()
+	pheromone.Reduce()
+	pheromone.AddPheromone(0, 3, 0.5)
+	colony.Initialize()
+	fmt.Println(pheromone.Pheromone)
 }
